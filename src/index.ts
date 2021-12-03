f2c45a612e5852f80ebf51b5dbc35827cc102431
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2021-12-03 10:05:26 -0800

    Fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default class VantaServerlessPlugin {
   constructor(serverless: any) {
     // In later versions of serverless the service is just a string
-    const serivceName =
+    const serviceName =
       (serverless?.service?.service as any)?.name || serverless.service.service;
 
     serverless.service.provider = {
@@ -11,7 +11,7 @@ export default class VantaServerlessPlugin {
         ...(serverless.service.provider?.deploymentBucketObject ?? {}),
         blockPublicAccess: true, // This is by defualt true in serverless due to ACLs and policies, but we want to be explicit for Vanta
         tags: {
-          VantaDescription: `A bucket which holds the source code for the ${serivceName} service used while deploying the lambda functions`,
+          VantaDescription: `A bucket which holds the source code for the ${serviceName} service used while deploying the lambda functions`,
           VantaContainsUserData: "false", // Only source code no user data
           VantaNonProd: (
             serverless.service.provider.stage !== "prod"
