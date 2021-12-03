719e3a673d6971dc576e7a4c08f45b9c5f0312e2
Cosmo Wolfe <cosmo.wolfe@gmail.com>
2021-12-03 09:56:55 -0800

    Fix tests and lint

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 export default class VantaServerlessPlugin {
   constructor(serverless: any) {
     // In later versions of serverless the service is just a string
-    const serivceName = (serverless?.service?.service as any)?.name || serverless.service.service;
+    const serivceName =
+      (serverless?.service?.service as any)?.name || serverless.service.service;
 
     serverless.service.provider = {
-      ...serverless.service.provider ?? {},
+      ...(serverless.service.provider ?? {}),
       deploymentBucketObject: {
         name: null,
-        ...serverless.service.provider?.deploymentBucketObject ?? {},
+        ...(serverless.service.provider?.deploymentBucketObject ?? {}),
         blockPublicAccess: true, // This is by defualt true in serverless due to ACLs and policies, but we want to be explicit for Vanta
         tags: {
           VantaDescription: `A bucket which holds the source code for the ${serivceName} service used while deploying the lambda functions`,
           VantaContainsUserData: "false", // Only source code no user data
-          VantaNonProd: (serverless.service.provider.stage !== 'prod').toString(),
-          ...serverless.service.provider?.deploymentBucketObject?.tags ?? {},
-        }
-      }
-    }
+          VantaNonProd: (
+            serverless.service.provider.stage !== "prod"
+          ).toString(),
+          ...(serverless.service.provider?.deploymentBucketObject?.tags ?? {}),
+        },
+      },
+    };
   }
 }
